fb41fdeb34a45e9c98ab4ac19cf39f085428bffd
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2016-12-09 09:06:19 -0500

    Reorganize plotting code

--- data-raw/create_map_plot.R
+++ data-raw/create_map_plot.R
@@ -10,12 +10,11 @@ library(ggthemes)
 load("../data/us_state_20m.rda")
 
 blank_map <- ggplot(data = map) + 
-  geom_map(map = map, aes(x = long, y = lat, map_id = id, group = group), colour = "black", fill = "white", size = 0.3) +
-  coord_equal() +
-  theme_map() + 
+  geom_map(map = map, aes(x = long, y = lat, map_id = id, group = group), 
+           colour = "black", fill = "white", size = 0.3) +
+  coord_equal() + theme_map() + 
   guides(fill = guide_legend(override.aes = list(colour = NULL))) +
-  scale_x_continuous(breaks = NULL) + 
-  scale_y_continuous(breaks = NULL) +
+  scale_x_continuous(breaks = NULL) + scale_y_continuous(breaks = NULL) +
   labs(title = paste0("U.S. State Map")) +
   theme(plot.title = element_text(size = 22, face = "bold"))
 
