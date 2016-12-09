970acd9b0d1ed1ae94dc9afd6d17587eca2bd190
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2016-12-09 09:13:20 -0500

    Remove unnecessary ggplot code
    Was leftover from a previous map plotting script.

--- data-raw/create_map_plot.R
+++ data-raw/create_map_plot.R
@@ -13,9 +13,7 @@ blank_map <- ggplot(data = map) +
   geom_map(map = map, aes(x = long, y = lat, map_id = id, group = group), 
            colour = "black", fill = "white", size = 0.3) +
   coord_equal() + theme_map() + 
-  guides(fill = guide_legend(override.aes = list(colour = NULL))) +
-  scale_x_continuous(breaks = NULL) + scale_y_continuous(breaks = NULL) +
-  labs(title = paste0("U.S. State Map")) +
+  labs(title = "U.S. State Map") +
   theme(plot.title = element_text(size = 22, face = "bold"))
 
 ggsave("../blank-us-map.png", blank_map, width = 6, height = 4.9)
