ed04348d1c616e71b53b08f036d9ba5840df6d21
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2016-12-12 22:49:50 -0500

    Add test for us_map invalid region error

--- tests/testthat/test_usmap.R
+++ tests/testthat/test_usmap.R
@@ -5,3 +5,7 @@ states_map <- us_map(region = "states")
 test_that("length of us_map states is 51", {
   expect_equal(length(unique(states_map$id)), 51)
 })
+
+test_that("error occurs for invalid region", {
+  expect_error(us_map(region = "cities"))
+})
