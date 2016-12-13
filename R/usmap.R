cd9848b6640c08903fb59be89af5448ff91e0064
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2016-12-12 22:38:50 -0500

    Add documentation for us_map function

--- R/usmap.R
+++ R/usmap.R
@@ -1,4 +1,13 @@
-
+#' Retrieve US map data
+#' 
+#' @param region The region breakdown for the map, either `states` or `counties`.
+#' @param include The regions to include in the output data frame. The regions
+#'  must be of the same type as `region`.
+#'  
+#' @return A data frame of US map coordinates divided by the desired `region`.
+#' @examples
+#' states_map <- us_map(region = "states")
+#' counties_map <- us_map(region = "counties")
 us_map <- function(region, include = c()) {
   if (region %in% c("states", "counties")) {
     load(system.file("extdata", paste0("us_", region, ".rda"), package = "usmap"))
