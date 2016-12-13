8e2556c6cdc1e18791f59ee9a7cfedc5a0ca29a3
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2016-12-12 22:31:43 -0500

    Add US map loading function

--- R/usmap.R
+++ R/usmap.R
@@ -1,4 +1,9 @@
 
 us_map <- function(region, include = c()) {
-  
+  if (region %in% c("states", "counties")) {
+    load(system.file("extdata", paste0("us_", region, ".rda"), package = "usmap"))
+    return(map)
+  } else {
+    stop("Region must be either `states` or `counties`.")
+  }
 }
