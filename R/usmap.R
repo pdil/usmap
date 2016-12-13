4f4950b64872a5eaf1ab9306bfdf3ab7ded076dd
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2016-12-13 11:46:53 -0500

    Make regions parameter plural

--- R/usmap.R
+++ R/usmap.R
@@ -1,13 +1,13 @@
 #' Retrieve US map data
 #' 
-#' @param region The region breakdown for the map, either `states` or `counties`.
+#' @param regions The region breakdown for the map, either `states` or `counties`.
 #' @param include The regions to include in the output data frame. The regions
 #'  must be of the same type as `region`.
 #'  
 #' @return A data frame of US map coordinates divided by the desired `region`.
 #' @examples
 #' print("Examples will go here.")
-us_map <- function(region, include = c()) {
+us_map <- function(regions, include = c()) {
   if (region %in% c("states", "counties")) {
     load(system.file("extdata", paste0("us_", region, ".rda"), package = "usmap"))
     
@@ -17,6 +17,6 @@ us_map <- function(region, include = c()) {
     
     return(map)
   } else {
-    stop("Region must be either `states` or `counties`.")
+    stop("Regions must be either `states` or `counties`.")
   }
 }
