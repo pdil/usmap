b48808fde1c5b368c8be9c8888d5a8954ebbe2a7
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2016-12-13 09:24:54 -0500

    Add `include` parameter functionality

--- R/usmap.R
+++ R/usmap.R
@@ -10,6 +10,11 @@
 us_map <- function(region, include = c()) {
   if (region %in% c("states", "counties")) {
     load(system.file("extdata", paste0("us_", region, ".rda"), package = "usmap"))
+    
+    if (length(include) > 0) {
+      map <- map[map$id %in% include, ]  
+    }
+    
     return(map)
   } else {
     stop("Region must be either `states` or `counties`.")
