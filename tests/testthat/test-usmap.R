d1e6611293653677b6b95c64fd59082b6b9783c3
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2016-12-13 11:36:56 -0500

    Add test for counties map

--- tests/testthat/test-usmap.R
+++ tests/testthat/test-usmap.R
@@ -1,10 +1,15 @@
 context("Loading US map data")
 
 states_map <- us_map(region = "states")
+counties_map <- us_map(region = "counties")
 
-test_that("length of us_map states is 51", {
+test_that("structure of states df is correct", {
   expect_equal(length(unique(states_map$id)), 51)
 })
+
+test_that("structure of counties df is correct", {
+  expect_equal(length(unique(counties_map$id)), 3142)
+}
 
 test_that("error occurs for invalid region", {
   expect_error(us_map(region = "cities"))
