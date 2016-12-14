39c03338205ed9ac5e46c0d9ad6e30b87b063253
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2016-12-13 22:22:47 -0500

    Convert uppercase state names to title case

--- data-raw/format-map-df.R
+++ data-raw/format-map-df.R
@@ -6,6 +6,8 @@ merged_states_df <- merge(states_map_df, states_fips, by.x = "id", by.y = "fips"
 final_states_df <- merged_states_df[, c("long", "lat", "order", "hole", "piece", "group", "id", "abbr", "full")]
 colnames(final_states_df) <- c("long", "lat", "order", "hole", "piece", "group", "fips", "abbr", "full")
 
+final_states_df$full <- tools::toTitleCase(tolower(final_states_df$full))
+
 write.csv(final_states_df, file = "us_states.csv", row.names = FALSE, na = "")
 
 write.csv(states_fips, file = "state_fips.csv", row.names = FALSE, na = "")
--- tests/testthat/test-fips.R
+++ tests/testthat/test-fips.R
@@ -2,7 +2,7 @@ context("Retrieving FIPS codes")
 
 test_that("returns correct FIPS code for NJ", {
   expect_match(fips(state = "NJ"), "34")
-  expect_match(fips(state = "NEW JERSEY"), "34")
+  expect_match(fips(state = "New Jersey"), "34")
 })
 
 # this will be replaced once county functionality is added
