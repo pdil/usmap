d5058e027821743af28f364b29d5e6f25eae104c
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2016-12-16 10:15:02 -0500

    Remove county FIPS test for now
    Will be re-added once the data file has been created.

--- tests/testthat/test-fips.R
+++ tests/testthat/test-fips.R
@@ -6,9 +6,9 @@ test_that("returns correct FIPS code for NJ", {
 })
 
 # this will be replaced once county functionality is added
-test_that("returns 0 when county is entered", {
-  expect_equal(fips(county = "Anything"), 0)
-})
+#test_that("returns 0 when county is entered", {
+#  expect_equal(fips(county = "Anything"), 0)
+#})
 
 test_that("error occurs for entering both state and county", {
   expect_error(fips(state = "NJ", county = "Mercer"))
