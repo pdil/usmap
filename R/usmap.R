da25fc555eb00cc7d6a3585a4a2249ea71bbc6f8
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2016-12-17 00:03:25 -0500

    Change import method for utils
    Improves readability.

--- R/usmap.R
+++ R/usmap.R
@@ -1,7 +1,5 @@
 #' Retrieve US map data
 #' 
-#' @importFrom "utils" "read.csv"
-#' 
 #' @param regions The region breakdown for the map, either `states` or `counties`.
 #' @param include The regions to include in the output data frame. The regions
 #'  must be of the same type as `regions`.
@@ -13,7 +11,7 @@
 #' @export
 us_map <- function(regions, include = c()) {
   if (regions %in% c("states", "counties")) {
-    df <- read.csv(system.file("extdata", paste0("us_", regions, ".csv"), package = "usmap"))
+    df <- utils::read.csv(system.file("extdata", paste0("us_", regions, ".csv"), package = "usmap"))
     
     if (length(include) > 0) {
       df <- df[df$full %in% include | df$abbr %in% include | sprintf("%02d", df$fips) %in% include, ]  
