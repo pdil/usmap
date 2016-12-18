473ce40501007344a59221ced7b95173a43a9742
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2016-12-18 11:25:14 -0500

    Add more robust fips_info test

--- tests/testthat/test-fips-info.R
+++ tests/testthat/test-fips-info.R
@@ -1,11 +1,16 @@
 context("Retrieving FIPS code info")
 
 test_that("returns correct state FIPS information", {
-  ak_result <- data.frame(abbr = "AK", fips = "02", full = "Alaska", stringsAsFactors = FALSE)
+  ak_result <- data.frame(
+    abbr = c("AK", "NJ"),
+    fips = c("02", "34"),
+    full = c("Alaska", "New Jersey"),
+    stringsAsFactors = FALSE
+  )
 
-  expect_equivalent(fips_info(2), ak_result)
-  expect_equivalent(fips_info("2"), ak_result)
-  expect_equivalent(fips_info("02"), ak_result)
+  expect_equivalent(fips_info(c(2, 34)), ak_result)
+  expect_equivalent(fips_info(c("2", "34")), ak_result)
+  expect_equivalent(fips_info(c("02", "34")), ak_result)
 })
 
 test_that("returns correct county FIPS information", {
@@ -19,6 +24,14 @@ test_that("returns correct county FIPS information", {
 
   expect_equivalent(fips_info(c(2016, 2020)), ak_result)
   expect_equivalent(fips_info(c("02016", "02020")), ak_result)
+})
+
+# non-existent yet valid means a FIPS such as "03", which falls
+# within the prescribed boundaries yet is not assigned to any US state
+# and therefore returns a warning
+test_that("warning occurs for non-existent yet valid FIPS", {
+  expect_warning(fips_info("03"))
+  expect_warning(fips_info("03055"))
 })
 
 test_that("error occurs for non-numeric/character FIPS", {
