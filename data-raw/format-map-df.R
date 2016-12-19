5c50bc1ad6c310e4f6d1cf04ccca1649a2d2edab
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2016-12-18 21:49:20 -0500

    Merge FIPS with county map data

--- data-raw/format-map-df.R
+++ data-raw/format-map-df.R
@@ -1,27 +1,44 @@
 
+# State FIPS ####
 source("states-fips.R")
-
-states_map_df <- readr::read_csv("us_states_raw.csv")
-
-merged_states_df <- merge(states_map_df, states_fips, by.x = "id", by.y = "fips", all.x = TRUE)
-final_states_df <- merged_states_df[, c("long", "lat", "order", "hole", "piece", "group", "id", "abbr", "full")]
-colnames(final_states_df) <- c("long", "lat", "order", "hole", "piece", "group", "fips", "abbr", "full")
-
-write.csv(final_states_df, file = "us_states.csv", row.names = FALSE, na = "")
 write.csv(states_fips, file = "state_fips.csv", row.names = FALSE, na = "")
 
-# County FIPS ####
+# Merge state FIPS with map ####
+states_map_df <- readr::read_csv("us_states_raw.csv")
+merged_states_df <- merge(states_map_df, states_fips,
+                          by.x = "id", by.y = "fips", all.x = TRUE)
 
+final_states_df <- merged_states_df[, c("long", "lat", "order", "hole", "piece",
+                                        "group", "id", "abbr", "full")]
+
+colnames(final_states_df) <- c("long", "lat", "order", "hole", "piece",
+                               "group", "fips", "abbr", "full")
+
+write.csv(final_states_df, file = "us_states.csv", row.names = FALSE, na = "")
+
+# County FIPS ####
 county_fips <- readr::read_csv("county_fips.txt", col_names = FALSE)
 colnames(county_fips) <- c("abbr", "state_fips", "county_fips", "county", "class_code")
 
 county_fips_merged <- merge(county_fips, states_fips, by = "abbr", all.x = TRUE)
-
 county_fips_final <- data.frame(
-  full = county_fips_merged$full, 
-  abbr = county_fips_merged$abbr, 
-  county = county_fips_merged$county, 
+  full = county_fips_merged$full,
+  abbr = county_fips_merged$abbr,
+  county = county_fips_merged$county,
   fips = paste0(county_fips_merged$state_fips, county_fips_merged$county_fips)
 )
 
 write.csv(county_fips_final, file = "county_fips.csv", row.names = FALSE, na = "")
+
+# Merge county FIPS with map ####
+counties_map_df <- readr::read_csv("us_counties_raw.csv")
+merged_counties_df <- merge(counties_map_df, county_fips_final,
+                            by.x = "id", by.y = "fips", all.x = TRUE)
+
+final_counties_df <- merged_counties_df[, c("long", "lat", "order", "hole", "piece",
+                                            "group", "id", "abbr", "full", "county")]
+
+colnames(final_counties_df) <- c("long", "lat", "order", "hole", "piece",
+                                 "group", "fips", "abbr", "full", "county")
+
+write.csv(final_counties_df, file = "us_counties.csv", row.names = FALSE, na = "")
--- tests/testthat/test-usmap.R
+++ tests/testthat/test-usmap.R
@@ -10,21 +10,21 @@ test_that("structure of states df is correct", {
 test_that("correct states are included", {
   include_abbr <- c("AK", "AL", "AZ")
   map_abbr <- us_map(regions = "states", include = include_abbr)
-  
+
   include_full <- c("Alaska", "Alabama", "Arizona")
   map_full <- us_map(regions = "states", include = include_full)
-  
+
   include_fips <- c("02", "01", "04")
   map_fips <- us_map(regions = "states", include = include_fips)
-  
+
   expect_equal(length(unique(map_abbr$fips)), length(include_abbr))
   expect_equal(length(unique(map_full$fips)), length(include_full))
   expect_equal(length(unique(map_fips$fips)), length(include_fips))
 })
 
-# test_that("structure of counties df is correct", {
-#   expect_equal(length(unique(counties_map$id)), 3142)
-# })
+test_that("structure of counties df is correct", {
+  expect_equal(length(unique(counties_map$id)), 3142)
+})
 
 test_that("error occurs for invalid region", {
   expect_error(us_map(regions = "cities"))
