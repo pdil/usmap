2953544c911550917c15fc07d2e4b1c2f1f3fbca
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2016-12-20 10:19:05 -0500

    Add include functionality for counties and allow singular regions value

--- R/usmap.R
+++ R/usmap.R
@@ -2,8 +2,10 @@
 #'
 #' @param regions The region breakdown for the map, either \code{"states"}
 #'  or \code{"counties"}.
-#' @param include The regions to include in the output data frame. The regions
-#'  must be of the same type as \code{regions}.
+#' @param include The regions to include in the output data frame. If \code{regions} is
+#'  \code{"states"}, the value can be either a state name, abbreviation or FIPS code. 
+#'  For counties, the FIPS must be provided as there can be multiple counties with the
+#'  same name.
 #'
 #' @return A data frame of US map coordinates divided by the desired \code{regions}.
 #' @examples
@@ -11,15 +13,27 @@
 #' plot(df$long, df$lat)
 #' @export
 us_map <- function(regions, include = c()) {
-  if (regions %in% c("states", "counties")) {
-    df <- utils::read.csv(system.file("extdata", paste0("us_", regions, ".csv"), package = "usmap"))
+  if (regions %in% c("states", "counties", "state", "county")) {
+    if (regions == "state") {
+      regions_ <- "states"
+    } else if (regions == "county") 
+      regions_ <- "counties"
+    } else {
+      regions_ <- regions
+    }
+    
+    df <- utils::read.csv(system.file("extdata", paste0("us_", regions_, ".csv"), package = "usmap"))
 
     if (length(include) > 0) {
-      df <- df[df$full %in% include | df$abbr %in% include | sprintf("%02d", df$fips) %in% include, ]
+      if (regions_ %in% ("counties", "county")) {
+        df <- df[df$full %in% include | df$abbr %in% include | sprintf("%02d", df$fips) %in% include, ]
+      } else {
+        df <- df[sprintf("%05d", df$fips) %in% include, ]
+      }
     }
 
-    return(df)
+    df
   } else {
-    stop("Regions must be either `states` or `counties`.")
+    stop("Regions must be either `states`, `state`, `counties`, or `county`.")
   }
 }
