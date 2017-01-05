6848c4e7817cf3c47f38cd9189c9fb95d262c7d8
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2017-01-05 11:18:10 -0500

    Add theme_map

--- R/plot-map.R
+++ R/plot-map.R
@@ -9,7 +9,7 @@
 #'   US map with the described parameters.
 #' 
 #' @export
-plot_usmap <- function(regions = "states", theme = ggthemes::theme_map()) {
+plot_usmap <- function(regions = "states", theme = theme_map()) {
   ggplot2::ggplot(
     data = us_map(regions = regions)
   ) + geom_polygon(
@@ -18,4 +18,27 @@ plot_usmap <- function(regions = "states", theme = ggthemes::theme_map()) {
     fill = "white",
     size = 0.4
   ) + theme
+}
+
+#' This creates a nice map theme. 
+#' It is borrowed from the ggthemes package located at this repository:
+#'  https://github.com/jrnold/ggthemes
+#' 
+#' This function was manually rewritten here to avoid the need for
+#'  another package import.
+#'
+#' @keywords internal
+theme_map <- function(base_size = 9, base_family = "") {
+  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme(axis.line = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title = element_blank(),
+          panel.background = element_blank(),
+          panel.border = element_blank(),
+          panel.grid = element_blank(),
+          panel.spacing = unit(0, "lines"),
+          plot.background = element_blank(),
+          legend.justification = c(0, 0),
+          legend.position = c(0, 0))
 }
