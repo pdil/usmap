9a4698651a3ff3b317181936d3dc7e4a691f0056
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2017-03-07 14:48:45 -0500

    Add customizable name for "values" column

--- R/map-with-data.R
+++ R/map-with-data.R
@@ -4,7 +4,10 @@
 #'   parameter should be a data frame consisting of two columns,
 #'   a fips code (2 characters for state, 5 characters for county)
 #'   and the value that should be associated with that region. The
-#'   columns of \code{data} _must_ be \code{fips} and \code{value}.
+#'   columns of \code{data} _must_ be \code{fips} and the value of the
+#'   `values` parameter.
+#' @param values The name of the column that contains the values to be associated
+#'   with a given region. The default is \code{"value"}.
 #' @param na The value to be inserted for states or counties that don't have
 #'   a value in \code{data}. This value must be of the same type as the \code{value}
 #'   column of \code{data}.
@@ -18,9 +21,9 @@
 #' state_data <- data.frame(fips = c("01", "02", "04"), value = c(1, 5, 8))
 #' map_with_data(state_data, na = 0)
 #' @export
-map_with_data <- function(data, na = NA) {
-  if (!is.data.frame(data) || ncol(data) != 2 || !("fips" %in% names(data)) || !("value" %in% names(data))) {
-    stop("`data` must be a data frame with two columns, `fips` and `value`")
+map_with_data <- function(data, values = "value", na = NA) {
+  if (!is.data.frame(data) || ncol(data) != 2 || !("fips" %in% names(data)) || !(values %in% names(data))) {
+    stop(paste0("`data` must be a data frame with two columns, `fips` and `", values, "`"))
   }
 
   if (length(data$fips) < 1) {
@@ -32,7 +35,7 @@ map_with_data <- function(data, na = NA) {
   map_df <- us_map(regions = ifelse(nchar(data$fips[1]) == 2, "state", "county"))
 
   result <- merge(map_df, data, by = "fips", all.x = TRUE, sort = FALSE)
-  result$value[is.na(result$value)] <- na
+  result[is.na(result[, values]), values] <- na
   result <- result[c(setdiff(names(result), names(data)), names(data))]
 
   result
