f7f9324c2f1cba76318efa04d35579919dffd14e
Paolo Di Lorenzo <dilorenzo@hey.com>
2023-12-10 14:34:48 -0500

    Add explicit package to testthat helper calls

--- tests/testthat/helper-requireNamespace-mock.R
+++ tests/testthat/helper-requireNamespace-mock.R
@@ -3,8 +3,8 @@
 # provided dependency package "pkg" to test that
 # error occurs when it's not installed.
 expect_package_error <- function(pkg, code, msg) {
-  with_mocked_bindings({
-    expect_error(code, paste0("`", pkg, "` must be installed"))
+  testthat::with_mocked_bindings({
+    testthat::expect_error(code, paste0("`", pkg, "` must be installed"))
   },
   requireNamespace = function(package, ...) package != pkg,
   .package = "base"
