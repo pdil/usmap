c8794a3de5ad76e69fb58ada41d5d58cb69bc400
Paolo Di Lorenzo <dilorenzo@hey.com>
2024-03-30 22:06:58 -0400

    Replace R-based version check with bash script in release.yaml

--- .github/workflow-resources/version-check.R
+++
@@ -1,6 +0,0 @@
-verify_version <- function(new_version) {
-    current_version <- desc::desc_get_version()
-    if (utils::compareVersion(new_version, current_version) != 1) {
-        Sys.setenv(INVALID_VERSION = "true")
-    }
-}
